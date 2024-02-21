e10d9d6dc50ed50a16518d65a9813401e1d1624d
Halil <halillkesgin@gmail.com>
2024-02-21 20:44:21 +0300

    item slider done

--- resources/views/front/_components/item.blade.php
+++ resources/views/front/_components/item.blade.php
@@ -1,115 +1,72 @@
-<div class="testimonial-slider">
-    <div id="carouselExampleControls" class="carousel carousel-dark" data-bs-ride="carousel">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="testimonial-title">
-                        <i class="bi bi-quote display-2"></i>
-                        <h2 class="fw-bold display-6">What our customers say</h2>
+<div class="my-4">
+
+    <div class="gray-box"></div>
+    <div class="container-xxl ">
+        <div class="owl-carousel" id="slider">
+            <div class="content-area">
+                <div class="relative">
+                    <div class="item-header text-white z-10">
+                        <h5 class="text-thin">
+                            Şıklığı Banyonuza Yansıtın
+                        </h5>
+                        <h1>
+                            Free Standing Lavabo
+                        </h1>
+
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+                    <div class="abs-right">
+                        <button class="item-button">
+                            <span>İncele</span>
+                            <img src="{{ asset('img/green-arrow-right.svg') }}" alt="">
+                        </button>
+                    </div>
+                    <div class="overlay"></div>
+                    <img src="{{ asset('img/item-1.svg') }}" alt="">
                 </div>
-                <div class="col-md-8">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
+            </div>
+            <div class="content-area">
+                <div class="relative">
+                    <div class="item-header text-white z-10">
+                        <h5 class="text-thin">
+                            Eşsiz tasarımı ile banyolarınıza
+                            modern bir görünüm kazandıracak
+                        </h5>
+                        <h1>
+                            4 Life Serisi
+                        </h1>
 
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="{{ asset('img/item-1.svg') }}" class="d-block w-100"
-                                        alt="...">
-                                </div>
-                            </div>
-                        </div>
                     </div>
+                    <div class="abs-right">
+                        <button class="item-button">
+                            <span>İncele</span>
+                            <img src="{{ asset('img/green-arrow-right.svg') }}" alt="">
+                        </button>
+                    </div>
+                    <div class="overlay"></div>
+                    <img src="{{ asset('img/item-2.svg') }}" alt="">
+                </div>
+            </div>
+            <div class="content-area">
+                <div class="relative">
+                    <div class="item-header text-white z-10">
+                        <h5 class="text-thin">
+                            Üstün hijyen teknolojisi ile
+                            favoriniz olacak
+                        </h5>
+                        <h1>
+                            Rita Serisi
+                        </h1>
+                    </div>
+                    <div class="abs-right">
+                        <button class="item-button">
+                            <span>İncele</span>
+                            <img src="{{ asset('img/green-arrow-right.svg') }}" alt="">
+                        </button>
+                    </div>
+                    <div class="overlay"></div>
+                    <img src="{{ asset('img/item-3.svg') }}" alt="">
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
-<script>
-    $(document).ready(function() {
-        var multipleCardCarousel = document.querySelector(
-            "#carouselExampleControls"
-        );
-        if (window.matchMedia("(min-width: 576px)").matches) {
-            var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-                interval: false,
-                wrap: false
-            });
-            var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-            var cardWidth = $(".carousel-item").width();
-            var scrollPosition = 0;
-            $("#carouselExampleControls .carousel-control-next").on("click", function() {
-                if (scrollPosition < carouselWidth - cardWidth * 3) {
-                    scrollPosition += cardWidth;
-                    $("#carouselExampleControls .carousel-inner").animate({
-                            scrollLeft: scrollPosition
-                        },
-                        600
-                    );
-                }
-            });
-            $("#carouselExampleControls .carousel-control-prev").on("click", function() {
-                if (scrollPosition > 1) {
-                    scrollPosition -= cardWidth;
-                    $("#carouselExampleControls .carousel-inner").animate({
-                            scrollLeft: scrollPosition
-                        },
-                        600
-                    );
-                }
-            });
-        } else {
-            $(multipleCardCarousel).addClass("slide");
-        }
-    });
-</script>
--- resources/views/front/layout/scripts.blade.php
+++ resources/views/front/layout/scripts.blade.php
@@ -1,4 +1,26 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src={{ 'js/custom.js' }}></script>
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script>
+    $("#slider").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        dots: true,
+        stagePadding: 2,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+    })
+</script>
